faebca5bae00bafb0e2917d91eca17315784e3fc
Hau Nguyen <haunguyen1064@gmail.com>
2021-07-13 15:50:58 +0700

    finish build

---
+++ build/precache-manifest.8832e654fa54c21c6a37b168213f3caf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c76c25392b9e801f4771622134d24c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "976d788c7ccd7a1f741e",
+    "url": "/static/js/2.818c99f4.chunk.js"
+  },
+  {
+    "revision": "245b031720f9884a8c36",
+    "url": "/static/js/main.489701e6.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
