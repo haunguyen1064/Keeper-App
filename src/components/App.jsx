ebbf5351705bb2b0b8e39944c291e583d8579424
Hau Nguyen <haunguyen1064@gmail.com>
2021-07-27 19:09:03 +0700

    mobile version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,8 +5,19 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 import {Route} from "react-router-dom"
 
+const text = <>Some features of this app:
+<ul>
+  <li>Create new notes.</li>
+  <li>Delete old notes.</li>
+  <li>Text box expands/closes automatically when clicking inside/outside the box.</li>
+  <li>Form validation, can't add an empty note.</li>
+</ul></>
+
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState([{
+    title: "Welcome!",
+    content: text
+  }]);
 
   function addNote(newNote) {
     setNotes((prevNotes) => {
